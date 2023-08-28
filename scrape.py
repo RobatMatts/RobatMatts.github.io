660de7527ec03c0d1f3ef573f95865fb8986afc5
RobatMatts <rob@mattswholesale.com>
2023-08-27 20:17:10 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -24,21 +24,27 @@ except FileNotFoundError:
 
 # Find the new content by comparing with previous content
 if new_text_message != previous_content:
-    # Send message to Discord webhook
-    data = {"content": "@everyone\n" + new_text_message}
-    response = requests.post(webhook_url, json=data)
-    
-    if response.status_code == 204:
-        print("New text update sent to Discord.")
+    # Find the difference between new and previous content
+    new_content_to_send = new_text_message.replace(previous_content, '').strip()
+
+    if new_content_to_send:
+        # Send message to Discord webhook
+        data = {"content": "@everyone\n" + new_content_to_send}
+        response = requests.post(webhook_url, json=data)
+
+        if response.status_code == 204:
+            print("New text update sent to Discord.")
+        else:
+            print("Failed to send new text update to Discord. Status code:", response.status_code)
+
+        # Update the previous_content.txt file with new content
+        with open("previous_content.txt", "w") as f:
+            f.write(new_text_message)
+
+        # Save the new content to debug.txt for troubleshooting
+        with open("debug.txt", "w", encoding="utf-8") as debug_file:
+            debug_file.write(new_content_to_send)
     else:
-        print("Failed to send new text update to Discord. Status code:", response.status_code)
-
-    # Update the previous_content.txt file with new content
-    with open("previous_content.txt", "w") as f:
-        f.write(new_text_message)
-
-    # Save the new content to debug.txt for troubleshooting
-    with open("debug.txt", "w", encoding="utf-8") as debug_file:
-        debug_file.write(new_text_message)
+        print("No new text to send.")
 else:
     print("No new text to send.")
