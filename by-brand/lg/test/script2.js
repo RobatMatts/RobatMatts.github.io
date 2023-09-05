2473a0891357579496d847054165a4d5bb662881
RobatMatts <rob@mattswholesale.com>
2023-09-05 02:14:12 -0400

    d

---
+++ by-brand/lg/test/script2.js
@@ -0,0 +1,16 @@
+// Access and display JSON data
+const outputDiv = document.getElementById("output");
+
+// Fetch JSON data from the external file
+fetch("data2.json")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    outputDiv.innerHTML = `
+      <p>Name: ${jsonData.name}</p>
+      <p>Age: ${jsonData.age}</p>
+      <p>City: ${jsonData.city}</p>
+    `;
+  })
+  .catch((error) => {
+    console.error("Error fetching JSON data:", error);
+  });
