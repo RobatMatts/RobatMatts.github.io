a4181a08c046622400fbb9ee3ab929f64a74a657
RobatMatts <rob@mattswholesale.com>
2023-09-05 16:28:41 -0400

    Update script5.js

--- by-brand/lg/script5.js
+++ by-brand/lg/script5.js
@@ -4,7 +4,7 @@ function createProductBlock(product) {
   productBlock.classList.add("item", "featured-product");
 
   productBlock.innerHTML = `
-    <a class="block" href="../../products/lg/dlgx7801we.html" title="View ${product.name}">
+    <a class="block" href="../../products/${Product.Brand}" title="View ${product.Desc}">
       <div class="model-img">
         <!-- Flags and Image -->
         <!-- ... -->
@@ -14,11 +14,11 @@ function createProductBlock(product) {
           <img class="model-brand-logo" height="40" title="LG Logo" alt="LG Logo" src="../../common_images/catalog_logos_standard/LG20e8.GIF?ccid=x350d5ebd">
         </p>
         <p class="model-desc">
-          <strong class="related-item-desc dotdotdot">${product.name}</strong>
+          <strong class="related-item-desc dotdotdot">${product.Desc}</strong>
         </p>
         <p class="model-branding-wrapper small text-muted">
-          <strong class="model-brand model-brand-text">${product.brand}</strong> 
-          <span class="model-number truncate">${product.modelNumber}</span>
+          <strong class="model-brand model-brand-text">${product.Brand}</strong> 
+          <span class="model-number truncate">${product.PN}</span>
         </p>
         <div class="model-detail__lower">	
           <!-- Rating and Price Information -->
